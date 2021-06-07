aedf4a2e63ca73339269ece29fe080fe215c15c0
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-06-07 10:34:04 +0100

    search works

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
-
 let charctersList = document.getElementById('charactersList');
 let searchBar = document.getElementById('searchBar');
+let hpCharacters = [];
+
 
 searchBar.addEventListener('keyup', (e) => {
-    console.log(e.target.value);
+    const searchString = e.target.value;
+    const filteredCharacters = hpCharacters.filter(character => {
+        return character.name.includes(searchString) || character.house.includes(searchString)
+    })
+    console.log(filteredCharacters);
+    displayCharacters(filteredCharacters)
 });
 
-console.log(searchBar);
 const loadCharacters = async () => {
     try {
         const response = await fetch("https://hp-api.herokuapp.com/api/characters");
-        let hpCharacters = await response.json();
+        hpCharacters = await response.json();
         console.log(hpCharacters);
-
-        const displayCharacters = (hpCharacters) => {
-            const htmlString = hpCharacters.map(character => {
-                return `
-                    <li class="character">
-                        <h2>${ character.name }</h2>
-                        <p>House: ${ character.house }</p>
-                        <img src="${ character.image }"/>
-                    </li>
-                `
-            })
-
-            charctersList.innerHTML = htmlString;
-        }
         displayCharacters(hpCharacters)
     }
-
-    catch(err) {
+    catch (err) {
         console.log(err)
     }
+}
+
+const displayCharacters = (hpCharacters) => {
+    const htmlString = hpCharacters.map(character => {
+        return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"/>
+            </li>
+        `
+    })
+    charctersList.innerHTML = htmlString;
 }
 
 loadCharacters();
